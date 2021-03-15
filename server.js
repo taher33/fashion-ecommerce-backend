1bdf91bdd6ad71f56ea32e0fefa060af6224036b
taher33 <taherhehe@gmail.com>
2021-03-15 20:12:44 +0100

    ...

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://fashion-ecommerce-lime.vercel.app/",
   })
 );
 
