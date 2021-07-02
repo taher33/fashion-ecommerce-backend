6ca0e2bceccf8efa8cc632dbbb6b1b41ce4585f3
taher33 <taherhehe@gmail.com>
2021-07-02 19:43:34 +0100

    ....

--- controller/authController.js
+++ controller/authController.js
@@ -6,7 +6,7 @@ const appError = require("../utils/appError");
 
 const signToken = (id) => {
   secure = false;
-  if (process.env.NODE_ENV === prod) secure = true;
+  if (process.env.NODE_ENV === "prod") secure = true;
   return jwt.sign({ id }, process.env.JWT_SECRET || "cute cat", {
     expiresIn: process.env.JWT_EXPIRES_IN || 3600 * 1000 * 24,
     secure,
