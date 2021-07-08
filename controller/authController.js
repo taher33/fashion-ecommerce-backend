ffb466a1d0d55a3dc7682627e22a764cb1d18265
taher33 <taherhehe@gmail.com>
2021-07-08 21:55:51 +0100

    some fixes

--- controller/authController.js
+++ controller/authController.js
@@ -12,18 +12,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  secure = false;
-  if (process.env.NODE_ENV === "prod") secure = true;
+
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_IN * 3600 * 1000 || 3600 * 1000 * 55
     ),
     maxAge: process.env.JWT_COOKIE_IN * 3600 * 1000 || 3600 * 1000 * 55,
     httpOnly: true,
-    sameSite: "none",
-    secure,
   };
-  //   if (process.env.NODE_ENV === "prod") cookieOptions.secure = true;
+  if (process.env.NODE_ENV === "prod") {
+    cookieOptions.secure = true;
+    cookieOptions.sameSite = "none";
+  }
 
   res.cookie("jwt", token, cookieOptions);
 
